62089880c6f956c888912aa25b2a811666fc208f
Alexander Borovykh <immaculate.pine@gmail.com>
2014-10-24 13:18:47 +0400

    Install generator actions order fixed

--- lib/generators/superb_text_constructor/install_generator.rb
+++ lib/generators/superb_text_constructor/install_generator.rb
@@ -3,6 +3,10 @@ module SuperbTextConstructor
     # Generator that installs all the parts of gem
     class InstallGenerator < ::Rails::Generators::Base
 
+      def generate_config
+        generate 'superb_text_constructor:config'
+      end
+
       def generate_migrations
         generate 'superb_text_constructor:migrations'
       end
@@ -13,10 +17,6 @@ module SuperbTextConstructor
 
       def generate_initializer
         generate 'superb_text_constructor:initializer'
-      end
-
-      def generate_config
-        generate 'superb_text_constructor:config'
       end
 
     end
